415cb90f0890a39695c44c9e8db36c79a233b70e
Jacky WU <hongzhi.wu@139.com>
2018-11-12 20:27:44 +0800

    add ts demo

--- modules/globals/components/components1.js
+++ modules/globals/components/components1.js
@@ -1,4 +1,17 @@
-define(function(require, exports, module) {
-    var module = exports.module = angular.module('application.components.1', ['ng']);
-    
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "angular"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var angular = require("angular");
+    var module = angular.module('application.components.1', ['ng']);
+    exports.module = module;
+    var name = module.name;
+    exports.name = name;
 });
---
+++ modules/globals/components/components1.ts
@@ -0,0 +1,8 @@
+import * as angular from  'angular';
+
+var module = angular.module('application.components.1', ['ng']);
+var name : string = module.name;
+export {
+    name,
+    module
+}
